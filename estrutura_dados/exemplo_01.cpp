83254e4aca81cc5c73cc8b364ac2f361706aff58
nely de faria <nelly.nf54@gmail.com>
2021-06-03 14:32:37 -0300

    Adiciona arquivo exemplo_01.cpp

---
+++ estrutura_dados/exemplo_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void asterisco();
+
+int main()
+{
+    asterisco();
+
+    return 0;
+}
+
+void asterisco() {
+
+    for (int i = 0; i < 20; i++) {
+
+        cout << "*" << endl;
+    }
+}

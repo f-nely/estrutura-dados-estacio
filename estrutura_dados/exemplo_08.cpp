2b9b8dd96bdb77ce604da64f2278f82b1dbfefd0
nely de faria <nelly.nf54@gmail.com>
2021-06-03 14:37:28 -0300

    Adiciona arquivo exemplo_08.cpp

---
+++ estrutura_dados/exemplo_08.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vetor[5] = {18, 23, 52, 13, 15};
+    cout << "\nValor do primeiro elemento: " << vetor[0] << endl;
+    cout << "\nEndereço do primeiro elemento: " << &vetor[0] << endl;
+    cout << "\nEndereço do veto: " << &vetor<< endl;
+    cout << "\nVetor: " << vetor << endl;
+
+    return 0;
+}

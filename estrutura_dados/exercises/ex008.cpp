67340650ec77f66e000fe25e2e56fe41f68cc060
nely de faria <nelly.nf54@gmail.com>
2021-06-09 16:47:57 -0300

    Adiciona arquivo ex008.cpp

---
+++ estrutura_dados/exercises/ex008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 15;
+
+    if (x > 0) {
+        int x;
+        x = 25;
+        cout << x << endl;
+    }
+
+    cout << x << endl;
+
+    return 0;
+}

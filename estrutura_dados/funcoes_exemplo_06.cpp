47f1423a57a018eb164a053addd02e42ef2cf40b
nely de faria <nelly.nf54@gmail.com>
2021-06-20 16:30:39 -0300

    Adiciona arquivo funcoes_exemplo_06.cpp

---
+++ estrutura_dados/funcoes_exemplo_06.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vet[] = {13, 23, 62, 50};
+
+    cout << "\nEndereço do vetor: " << vet << endl;
+    cout << "\nEndereço do vetor: " << &vet << endl;
+
+    return 0;
+}
